7b9c448d18aa3a1024419a053c4806bd5b2b38fe
William Desportes <williamdes@wdes.fr>
2019-06-08 15:11:24 +0200

    feat(scripts): Add scripts to generate translations
    Signed-off-by: William Desportes <williamdes@wdes.fr>

---
+++ example/simple.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+// Can be removed :)
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use \Wdes\PIL\plugins\MoReader;
+use \Wdes\PIL\Launcher;
+use \Wdes\PIL\Twig\Extension\I18n as ExtensionI18n;
+use \Twig\Environment as TwigEnvironment;
+use \Twig\Loader\FilesystemLoader as TwigLoaderFilesystem;
+
+$dataDir  = __DIR__ . '/locale/';
+$moReader = new MoReader(
+    ["localeDir" => $dataDir]
+);
+$moReader->readFile($dataDir . "fr.mo"); // Load the file you want (a sepecific language for example)
+Launcher::$plugin = $moReader;
+
+$loader = new TwigLoaderFilesystem([ __DIR__ . '/templates/' ]); // Load all templates from the dir
+$twig   = new TwigEnvironment($loader);
+
+$twig->addExtension(new ExtensionI18n());
+echo $twig->render(
+    'homepage.twig', // Can be found in the templates directory
+    [
+        'keyForTwig' => 'theValue', // Just an example line ;)
+        'say' => 'Hello world'
+    ]
+);
